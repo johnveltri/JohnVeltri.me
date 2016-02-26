781854eb1f8afc6138c52344a8a8dc011dfaa283
johnveltri <jveltri11@gmail.com>
2016-02-25 18:41:01 -0600

    Full Website Upload
    Live at JohnVeltri.me as of 2/25/2016

---
+++ sparklingchild/footer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package sparkling
+ */
+?>
+			</div><!-- close .*-inner (main-content or sidebar, depending if sidebar is used) -->
+		</div><!-- close .row -->
+	</div><!-- close .container -->
+</div><!-- close .site-content -->
+<footer>
+<div class="footer">
+	<div class="container">
+		<div>
+			<div class="col-xs-0 col-sm-2"></div>
+			<div class="col-xs-5 col-sm-2 footer-nav">
+				<h5><strong>Explore</strong></h5>
+				<a href="http://johnveltri.me/"><h6>Home</h6></a>
+				<a href="http://johnveltri.me/experience/"><h6>Experience</h6></a>
+				<a href="http://johnveltri.me/about/"><h6>About</h6></a>
+				<a href="http://johnveltri.me/connect/"><h6>Connect</h6></a>
+				<a href="http://johnveltri.me/blog/"><h6>Blog</h6></a>
+				<a href="http://johnveltri.me/Resume-John-Veltri.pdf" target="_blank" rel="nofollow"><h6>Resume</h6></a>
+			</div>
+			<div class="col-xs-2 col-sm-4"></div>
+			<div class="col-xs-5 col-sm-2 footer-nav footer-social">
+				<h5><strong>Get In Touch</strong></h5>
+				<a href="https://www.linkedin.com/in/johnaveltri" target="_blank"><h6 id="footer-linkedin">LinkedIn</h6></a>
+				<a href="https://twitter.com/johnaveltri" target="_blank"><h6 id="footer-twitter">Twitter</h6></a>
+				<a href="http://www.facebook.com/veltrija" target="_blank"><h6 id="footer-facebook">Facebook</h6></a>
+				<a href="mailto:&#106;&#118;&#101;&#108;&#116;&#114;&#105;&#49;&#49;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;" target="_blank"><h6 id="footer-email">Email</h6></a>
+			</div>
+			<div class="col-xs-0 col-sm-2"></div>
+		</div>
+		<div class="copyright col-xs-12">
+			<h6>&COPY; 2016. John Veltri.  All Rights Reserved.</h6>
+		</div>
+		<div class="footer-logo col-xs-12">
+			<b class="glyphicon glyphicon-chevron-right"></b>
+			<h6 id="footer-logo"> JOHN VELTRI</h6>
+		</div>
+	</div>
+</div>
+			<div class="scroll-to-top"><i class="fa fa-angle-up"></i></div><!-- .scroll-to-top -->
+		</footer><!-- #colophon -->
+	</div>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
